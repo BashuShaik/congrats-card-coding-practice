22227e3274438b39642a1754b5cee276addacb34
BashuShaik <sb9333171@gmail.com>
2023-01-15 07:01:51 +0000

    Congrats card Coding-practice

---
+++ index.js
@@ -0,0 +1,24 @@
+const element = (
+  // Write your code here.
+  <div class="bg-container">
+    <h1 class="heading">Congratulations</h1>
+
+    <div class="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="image"
+      />
+      <h1>Kiran V</h1>
+      <p>
+        Vishnu institute of Computer Education and Technology <br />
+        Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
